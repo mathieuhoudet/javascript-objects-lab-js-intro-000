05658a3aec8dd74fabee84b7a1902f28b6d2acfd
Mathieu Houdet <mathieu.houdet@gmail.com>
2019-03-03 22:27:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var recipes = new Object();
 
 function updateObjectWithKeyAndValue(object, key, value){
-  object[key] = value;
-  return obj;
+  return Object.assign({}, object, {[key]: value});
 }
